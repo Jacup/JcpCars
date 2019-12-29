835e15e6a56dcf65367811e7e90ca63afb21aeb8
Jakub Gramburg <jacubgr@gmail.com>
2019-12-29 16:45:23 +0100

    Add project files.

---
+++ JCPCars/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="JCPCars.MvcApplication" Language="C#" %>
---
+++ JCPCars/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(JCPCars.Startup))]
+namespace JCPCars
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
