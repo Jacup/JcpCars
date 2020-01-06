022db090e2fd33d31083232407fd342205071672
Jakub Gramburg <jacubgr@gmail.com>
2020-01-06 21:44:55 +0100

    login and register vm, views, controller

---
+++ JCPCars/ViewModels/AccountViewModels.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace JCPCars.ViewModels
+{
+    public class LoginViewModel
+    {
+        [Required(ErrorMessage = "Wprowadź prawidłowy adres email") ]
+        [EmailAddress]
+        public string Email { get; set; }
+
+        [Required(ErrorMessage = "Wprowadź hasło")]
+        public string Password { get; set; }
+        public bool RememberMe { get; set; }
+    }
+
+    public class RegisterViewModel
+    {
+        [Required]
+        [EmailAddress]
+        [Display(Name = "Email") ]
+        public string Email { get; set; }
+
+
+        [Required]
+        [StringLength(100, ErrorMessage = "The {0} must be at least {2} characters long", MinimumLength = 6)]
+        [DataType(DataType.Password)]
+        [Display(Name = "Hasło")]
+        public string Password { get; set; }
+
+
+        [DataType(DataType.Password)]
+        [Display(Name = "Potwierdź hasło")]
+        [Compare("Password", ErrorMessage = "Hasła nie są identyczne. " )]
+        public string ConfirmPassword { get; set; }
+
+
+    }
+
+}
