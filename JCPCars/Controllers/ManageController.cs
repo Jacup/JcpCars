71ddf6e38bd252b342fdb607699adea530cd0de0
Jakub Gramburg <jacubgr@gmail.com>
2020-01-08 19:18:45 +0100

    not working manage panel

---
+++ JCPCars/Controllers/ManageController.cs
@@ -0,0 +1,436 @@
+﻿using JCPCars.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Microsoft.AspNet.Identity;
+using Microsoft.AspNet.Identity.Owin;
+using Microsoft.Owin.Security;
+using System.Threading.Tasks;
+using System.Data.Entity;
+using JCPCars.DAL;
+using System.IO;
+using JCPCars.Infrastructure;
+using JCPCars.ViewModels;
+using System.Net;
+//using Hangfire;
+
+namespace JCPCars.Controllers
+{
+    [Authorize]
+    public class ManageController : Controller
+    {
+        StoreContext db = new StoreContext();
+
+        //private IMailService mailService;
+
+        //public ManageController(StoreContext context, IMailService mailService)
+        //{
+        //    this.mailService = mailService;
+        //    this.db = context;
+        //}
+
+        public ManageController(ApplicationUserManager userManager)
+        {
+            UserManager = userManager;
+        }
+
+        public enum ManageMessageId
+        {
+            ChangePasswordSuccess,
+            SetPasswordSuccess,
+            RemoveLoginSuccess,
+            LinkSuccess,
+            Error
+        }
+
+        private ApplicationUserManager _userManager;
+        public ApplicationUserManager UserManager
+        {
+            get
+            {
+                return _userManager ?? HttpContext.GetOwinContext().GetUserManager<ApplicationUserManager>();
+            }
+            private set
+            {
+                _userManager = value;
+            }
+        }
+
+        private IAuthenticationManager AuthenticationManager
+        {
+            get
+            {
+                return HttpContext.GetOwinContext().Authentication;
+            }
+        }
+
+        private bool HasPassword()
+        {
+            var user = UserManager.FindById(User.Identity.GetUserId());
+            if (user != null)
+            {
+                return user.PasswordHash != null;
+            }
+            return false;
+        }
+
+
+        // GET: Manage
+        public async Task<ActionResult> Index(ManageMessageId? message)
+        {
+            if (TempData["ViewData"] != null)
+            {
+                ViewData = (ViewDataDictionary)TempData["ViewData"];
+            }
+
+            if (User.IsInRole("Admin"))
+                ViewBag.UserIsAdmin = true;
+            else
+                ViewBag.UserIsAdmin = false;
+
+            var user = await UserManager.FindByIdAsync(User.Identity.GetUserId());
+            if (user == null)
+            {
+                return View("Error");
+            }
+            var userLogins = await UserManager.GetLoginsAsync(User.Identity.GetUserId());
+            var otherLogins = AuthenticationManager.GetExternalAuthenticationTypes().Where(auth => userLogins.All(ul => auth.AuthenticationType != ul.LoginProvider)).ToList();
+
+            var model = new ManageCredentialsViewModel
+            {
+                Message = message,
+                HasPassword = this.HasPassword(),
+                CurrentLogins = userLogins,
+                OtherLogins = otherLogins,
+                ShowRemoveButton = user.PasswordHash != null || userLogins.Count > 1,
+                //UserData = user.UserData
+            };
+
+            return View(model);
+        }
+
+        //[HttpPost]
+        //[ValidateAntiForgeryToken]
+        //public async Task<ActionResult> ChangeProfile([Bind(Prefix = "UserData")]UserData userData)
+        //{
+        //    if (ModelState.IsValid)
+        //    {
+        //        var user = await UserManager.FindByIdAsync(User.Identity.GetUserId());
+        //        user.UserData = userData;
+        //        var result = await UserManager.UpdateAsync(user);
+
+        //        AddErrors(result);
+        //    }
+
+        //    if (!ModelState.IsValid)
+        //    {
+        //        TempData["ViewData"] = ViewData;
+        //        return RedirectToAction("Index");
+        //    }
+
+        //    return RedirectToAction("Index");
+        //}
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<ActionResult> ChangePassword([Bind(Prefix = "ChangePasswordViewModel")]ChangePasswordViewModel model)
+        {
+            // In case we have simple errors - return
+            if (!ModelState.IsValid)
+            {
+                TempData["ViewData"] = ViewData;
+                return RedirectToAction("Index");
+            }
+
+            var result = await UserManager.ChangePasswordAsync(User.Identity.GetUserId(), model.OldPassword, model.NewPassword);
+            if (result.Succeeded)
+            {
+                var user = await UserManager.FindByIdAsync(User.Identity.GetUserId());
+                if (user != null)
+                {
+                    await SignInAsync(user, isPersistent: false);
+                }
+                return RedirectToAction("Index", new { Message = ManageMessageId.ChangePasswordSuccess });
+            }
+            AddErrors(result);
+
+            // In case we have login errors
+            if (!ModelState.IsValid)
+            {
+                TempData["ViewData"] = ViewData;
+                return RedirectToAction("Index");
+            }
+
+            var message = ManageMessageId.ChangePasswordSuccess;
+            return RedirectToAction("Index", new { Message = message });
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<ActionResult> SetPassword([Bind(Prefix = "SetPasswordViewModel")]SetPasswordViewModel model)
+        {
+            // In case we have simple errors - return
+            if (!ModelState.IsValid)
+            {
+                TempData["ViewData"] = ViewData;
+                return RedirectToAction("Index");
+            }
+
+            if (ModelState.IsValid)
+            {
+                var result = await UserManager.AddPasswordAsync(User.Identity.GetUserId(), model.NewPassword);
+                if (result.Succeeded)
+                {
+                    var user = await UserManager.FindByIdAsync(User.Identity.GetUserId());
+                    if (user != null)
+                    {
+                        await SignInAsync(user, isPersistent: false);
+                    }
+                    return RedirectToAction("Index", new { Message = ManageMessageId.SetPasswordSuccess });
+                }
+                AddErrors(result);
+
+                if (!ModelState.IsValid)
+                {
+                    TempData["ViewData"] = ViewData;
+                    return RedirectToAction("Index");
+                }
+            }
+
+            var message = ManageMessageId.SetPasswordSuccess;
+            return RedirectToAction("Index", new { Message = message });
+        }
+
+        //[HttpPost]
+        //[ValidateAntiForgeryToken]
+        //public async Task<ActionResult> RemoveLogin(string loginProvider, string providerKey)
+        //{
+        //    ManageMessageId? message;
+        //    var result = await UserManager.RemoveLoginAsync(User.Identity.GetUserId(), new UserLoginInfo(loginProvider, providerKey));
+        //    if (result.Succeeded)
+        //    {
+        //        var user = await UserManager.FindByIdAsync(User.Identity.GetUserId());
+        //        if (user != null)
+        //        {
+        //            await SignInAsync(user, isPersistent: false);
+        //        }
+        //        message = ManageMessageId.RemoveLoginSuccess;
+        //    }
+        //    else
+        //    {
+        //        message = ManageMessageId.Error;
+        //    }
+        //    return RedirectToAction("Index", new { Message = message });
+        //}
+
+        private void AddErrors(IdentityResult result)
+        {
+            foreach (var error in result.Errors)
+            {
+                ModelState.AddModelError("password-error", error);
+            }
+        }
+
+        private async Task SignInAsync(ApplicationUser user, bool isPersistent)
+        {
+            AuthenticationManager.SignOut(DefaultAuthenticationTypes.ExternalCookie, DefaultAuthenticationTypes.TwoFactorCookie);
+            AuthenticationManager.SignIn(new AuthenticationProperties { IsPersistent = isPersistent }, await user.GenerateUserIdentityAsync(UserManager));
+        }
+
+
+        //public ActionResult OrdersList()
+        //{
+        //    bool isAdmin = User.IsInRole("Admin");
+        //    ViewBag.UserIsAdmin = isAdmin;
+
+        //    IEnumerable<Order> userOrders;
+
+        //    // For admin users - return all orders
+        //    if (isAdmin)
+        //    {
+        //        userOrders = db.Orders.Include("OrderItems").
+        //            OrderByDescending(o => o.DateCreated).ToArray();
+        //    }
+        //    else
+        //    {
+        //        var userId = User.Identity.GetUserId();
+        //        userOrders = db.Orders.Where(o => o.UserId == userId).Include("OrderItems").
+        //            OrderByDescending(o => o.DateCreated).ToArray();
+        //    }
+
+        //    return View(userOrders);
+        //}
+
+        //[HttpPost]
+        //[Authorize(Roles = "Admin")]
+        //public OrderState ChangeOrderState(Order order)
+        //{
+        //    Order orderToModify = db.Orders.Find(order.OrderId);
+        //    orderToModify.OrderState = order.OrderState;
+        //    db.SaveChanges();
+
+        //    if (orderToModify.OrderState == OrderState.Shipped)
+        //    {
+        //        // Schedule confirmation
+        //        //string url = Url.Action("SendStatusEmail", "Manage", new { orderid = orderToModify.OrderId, lastname = orderToModify.LastName }, Request.Url.Scheme);
+
+        //        //BackgroundJob.Enqueue(() => Helpers.CallUrl(url));
+
+        //        //IMailService mailService = new HangFirePostalMailService();
+        //        //mailService.SendOrderShippedEmail(orderToModify);
+
+        //        mailService.SendOrderShippedEmail(orderToModify);
+
+        //        //dynamic email = new Postal.Email("OrderShipped");
+        //        //email.To = orderToModify.Email;
+        //        //email.OrderId = orderToModify.OrderId;
+        //        //email.FullAddress = string.Format("{0} {1}, {2}, {3}", orderToModify.FirstName, orderToModify.LastName, orderToModify.Address, orderToModify.CodeAndCity);
+        //        //email.Send();
+        //    }
+
+        //    return order.OrderState;
+        //}
+
+        //[AllowAnonymous]
+        //public ActionResult SendStatusEmail(int orderid, string lastname)
+        //{
+        //    // This could also be used (but problems when hosted on Azure Websites)
+        //    // if (Request.IsLocal)            
+
+        //    var orderToModify = db.Orders.Include("OrderItems").Include("OrderItems.Album").SingleOrDefault(o => o.OrderId == orderid && o.LastName == lastname);
+
+        //    if (orderToModify == null) return new HttpStatusCodeResult(HttpStatusCode.NotFound);
+
+        //    OrderShippedEmail email = new OrderShippedEmail();
+        //    email.To = orderToModify.Email;
+        //    email.OrderId = orderToModify.OrderId;
+        //    email.FullAddress = string.Format("{0} {1}, {2}, {3}", orderToModify.FirstName, orderToModify.LastName, orderToModify.Address, orderToModify.CodeAndCity);
+        //    email.Send();
+
+        //    return new HttpStatusCodeResult(HttpStatusCode.OK);
+        //}
+
+        //[AllowAnonymous]
+        //public ActionResult SendConfirmationEmail(int orderid, string lastname)
+        //{
+        //    // orderid and lastname as a basic form of auth
+
+        //    // Also might be called by scheduler (ie. Azure scheduler), pinging endpoint and using some kind of queue / db
+
+        //    // This could also be used (but problems when hosted on Azure Websites)
+        //    // if (Request.IsLocal)            
+
+        //    var order = db.Orders.Include("OrderItems").Include("OrderItems.Album").SingleOrDefault(o => o.OrderId == orderid && o.LastName == lastname);
+
+        //    if (order == null) return new HttpStatusCodeResult(HttpStatusCode.NotFound);
+
+        //    OrderConfirmationEmail email = new OrderConfirmationEmail();
+        //    email.To = order.Email;
+        //    email.Cost = order.TotalPrice;
+        //    email.OrderNumber = order.OrderId;
+        //    email.FullAddress = string.Format("{0} {1}, {2}, {3}", order.FirstName, order.LastName, order.Address, order.CodeAndCity);
+        //    email.OrderItems = order.OrderItems;
+        //    email.CoverPath = AppConfig.PhotosFolderRelative;
+        //    email.Send();
+
+        //    return new HttpStatusCodeResult(HttpStatusCode.OK);
+        //}
+
+        [Authorize(Roles = "Admin")]
+        public ActionResult AddProduct(int? carId, bool? confirmSuccess)
+        {
+            if (carId.HasValue)
+                ViewBag.EditMode = true;
+            else
+                ViewBag.EditMode = false;
+
+            var result = new EditProductViewModel();
+            var series = db.Series.ToArray();
+            result.Series = series;
+            result.ConfirmSuccess = confirmSuccess;
+
+            Car a;
+
+            if (!carId.HasValue)
+            {
+                a = new Car();
+            }
+            else
+            {
+                a = db.Cars.Find(carId);
+            }
+
+            result.Car = a;
+
+            return View(result);
+        }
+
+        [HttpPost]
+        public ActionResult AddProduct(HttpPostedFileBase file, EditProductViewModel model)
+        {
+            if (model.Car.CarId > 0)
+            {
+                // Saving existing entry
+
+                db.Entry(model.Car).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("AddProduct", new { confirmSuccess = true });
+            }
+            else
+            {
+                // Creating new entry
+
+                var f = Request.Form;
+                // Verify that the user selected a file
+                if (file != null && file.ContentLength > 0)
+                {
+                    // Generate filename
+
+                    var fileExt = Path.GetExtension(file.FileName);
+                    var filename = Guid.NewGuid() + fileExt;
+
+                    var path = Path.Combine(Server.MapPath(AppConfig.CarFolderRelative), filename);
+                    file.SaveAs(path);
+
+                    // Save info to DB
+                    model.Car.PictureFileName = filename;
+                    model.Car.DateAdded = DateTime.Now;
+
+                    db.Entry(model.Car).State = EntityState.Added;
+                    db.SaveChanges();
+
+                    return RedirectToAction("AddProduct", new { confirmSuccess = true });
+                }
+                else
+                {
+                    ModelState.AddModelError("", "Nie wskazano pliku.");
+                    var series = db.Series.ToArray();
+                    model.Series = series;
+                    return View(model);
+                }
+            }
+
+        }
+
+        public ActionResult HideProduct(int carId)
+        {
+            var car = db.Cars.Find(carId);
+            car.IsHidden = true;
+            db.SaveChanges();
+
+            return RedirectToAction("AddProduct", new { confirmSuccess = true });
+        }
+
+        public ActionResult UnhideProduct(int carId)
+        {
+            var car = db.Cars.Find(carId);
+            car.IsHidden = false;
+            db.SaveChanges();
+
+            return RedirectToAction("AddProduct", new { confirmSuccess = true });
+        }
+
+    }
+}
---
+++ JCPCars/ViewModels/ManageViewModels.cs
@@ -0,0 +1,63 @@
+﻿using JCPCars.Models;
+using Microsoft.AspNet.Identity;
+using Microsoft.Owin.Security;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace JCPCars.ViewModels
+{
+    public class ManageCredentialsViewModel
+    {
+        public bool HasPassword { get; set; }
+        public SetPasswordViewModel SetPasswordViewModel { get; set; }
+        public ChangePasswordViewModel ChangePasswordViewModel { get; set; }
+        public JCPCars.Controllers.ManageController.ManageMessageId? Message { get; set; }
+        public IList<UserLoginInfo> CurrentLogins { get; set; }
+        public IList<AuthenticationDescription> OtherLogins { get; set; }
+        public bool ShowRemoveButton { get; set; }
+
+    }
+    public class SetPasswordViewModel
+    {
+        [Required]
+        [StringLength(100, ErrorMessage = "The {0} must be at least {2} characters long.", MinimumLength = 6)]
+        [DataType(DataType.Password)]
+        [Display(Name = "New password")]
+        public string NewPassword { get; set; }
+
+        [DataType(DataType.Password)]
+        [Display(Name = "Confirm new password")]
+        [Compare("NewPassword", ErrorMessage = "The new password and confirmation password do not match.")]
+        public string ConfirmPassword { get; set; }
+    }
+
+    public class ChangePasswordViewModel
+    {
+        [Required]
+        [DataType(DataType.Password)]
+        [Display(Name = "Current password")]
+        public string OldPassword { get; set; }
+
+        [Required]
+        [StringLength(100, ErrorMessage = "The {0} must be at least {2} characters long.", MinimumLength = 6)]
+        [DataType(DataType.Password)]
+        [Display(Name = "New password")]
+        public string NewPassword { get; set; }
+
+        [DataType(DataType.Password)]
+        [Display(Name = "Confirm new password")]
+        [Compare("NewPassword", ErrorMessage = "The new password and confirmation password do not match.")]
+        public string ConfirmPassword { get; set; }
+    }
+
+    public class EditProductViewModel
+    {
+        public Car Car { get; set; }
+        public IEnumerable<Serie> Series { get; set; }
+        public bool? ConfirmSuccess { get; set; }
+    }
+}
